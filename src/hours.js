8bab0720463727bf9c650bbc78cc94be4a7a6599
alevitskiy <58774186+alevitskiy@users.noreply.github.com>
2020-01-28 12:36:51 +0300

    pull request

--- src/hours.js
+++ src/hours.js
@@ -1,18 +1,17 @@
 const data2 = [
-    {id: "6.1", customer: "Alex Smith", hours: "1"},
-    {id: "6.2", customer: "Henry Jones", hours: "3"},
-    {id: "6.3", customer: "Geoffrey Anderson", hours: "2.5"},
-    {id: "6.4", customer: "John Moore", hours: "4"},
-    {id: "6.5", customer: "William Thompson", hours: "1"},
-    {id: "6.6", customer: "Jane Clark", hours: "5"}
+    {id: "6.1", customer: "Alex Smith", hours: "1 h"},
+    {id: "6.2", customer: "Henry Jones", hours: "3 h"},
+    {id: "6.3", customer: "Geoffrey Anderson", hours: "2.5 h"},
+    {id: "6.4", customer: "John Moore", hours: "4 h"},
+    {id: "6.5", customer: "William Thompson", hours: "1 h"},
+    {id: "6.6", customer: "Jane Clark", hours: "5 h"}
 ]
 
 export const hoursSection = {
-    // type: "section", // invalid type (that's why background is white), only `template` supports it
     css:"webix_layout_custom",
+    minWidth: 230,
     padding: {
-        top: 5,
-        bottom: 10
+        top: 5
     },
     rows: [
         {
@@ -26,57 +25,27 @@ export const hoursSection = {
             ]
         },
         {
-            rows: [
-                {
-                    css: "list-header",
-                    height: 30,
-                    paddingX: 20,
-                    cols: [
-                        {
-                            template: "Name",
-                            type: "clean",
-                            css: "gray",
-                        },
-                        {
-                            template: "Time",
-                            type: "clean",
-                            css: "text-right gray"
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            paddingX: 20,
-            padding: {
-                top: 10
-            },
-            cols: [
-                {
-                    view: "list",
-                    borderless: true,
-                    autoheight: true,
-                    css: "custom-list-2",
-                    type: {
-                        height: 50,
-                        template(obj) {
-                            const html =
-                                `
-                                    <div class="list-col-2 semi-bold"> 
-                                        ${obj.customer}
-                                    </div>
+            view: "datatable",
+            borderless: true,
+            scroll:"y",
+            css: "custom-table",
+            headerRowHeight:30,
+            rowHeight:40,
+            columns:[
+                { 
+                    id:"customer", 
+                    header:{ text:"Name", css:"table-gray-header uppercase" },
+                    fillspace:true,
+                    css:"semi-bold"
+                },
+                { 
+                    id:"hours", 
+                    header:{ text:"Time", css:"text-right table-gray-header uppercase" },
+                    css:"text-right semi-bold"
 
-                                    <div class="list-col-2 text-right semi-bold"> 
-                                    ${obj.hours} h 
-                                    </div>
-                                `
-                            return html
-                        }
-                    },
-                    data: data2
                 }
-            ]
+            ],
+            data: data2
         },
-        {}
     ]
 }
