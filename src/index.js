8012449ac5ff36a729cfedcd60833af52bfd4339
listopad <olistopad@xbsoftware.com>
2020-02-27 17:19:31 +0300

    [update] eslint and styling

--- src/index.js
+++ src/index.js
@@ -9,47 +9,49 @@ import {hoursSection} from "./js/hours";
 import "./styles/main.scss";
 
 webix.ready(() => {
+	if (!webix.env.touch && webix.env.scrollSize) {
+		webix.CustomScroll.init();
+	}
 
-    if (!webix.env.touch && webix.env.scrollSize)
-        webix.CustomScroll.init();
-
-    webix.ui({
-        padding: 10,
-        type: "wide",
-        margin: 20,
-        rows: [
-            {
-                cols: [
-                    {
-                        view: "label",
-                        label: "Activities of the day",
-                        css: "font-16"
-                    },
-                    {
-                        view: "button",
-                        width: 150,
-                        align: "right",
-                        css: "webix_primary",
-                        label: "<span>Assignements</span><span class='webix_icon wxi-angle-down'></span>"
-                    }
-                ]
-            },
-            {
-                margin: 20,
-                cols: [
-                    actionsSection,
-                    assignmentsSection
-                ]
-            },
-            {
-                margin: 20,
-                cols: [
-                    balancesSection,
-                    invoicesSection,
-                    jobsSection,
-                    hoursSection
-                ]
-            }
-        ]
-    });
-}); 
+	webix.ui({
+		padding: 10,
+		type: "wide",
+		margin: 20,
+		rows: [
+			{
+				cols: [
+					{
+						view: "template",
+						type: "header",
+						css: "webix_clean font-18",
+						borderless: true,
+						template: "Activities of the day"
+					},
+					{
+						view: "button",
+						width: 150,
+						align: "right",
+						css: "webix_primary",
+						label: "<span>Assignements</span><span class='webix_icon wxi-angle-down'></span>"
+					}
+				]
+			},
+			{
+				margin: 20,
+				cols: [
+					actionsSection,
+					assignmentsSection
+				]
+			},
+			{
+				margin: 20,
+				cols: [
+					balancesSection,
+					invoicesSection,
+					jobsSection,
+					hoursSection
+				]
+			}
+		]
+	});
+});
